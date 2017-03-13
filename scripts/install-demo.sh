3a5a7181df66a69937612581a28833f7da7904d1
Matt Glaman <nmd.matt@gmail.com>
2017-03-13 14:20:10 -0500

    Remove demo from default json, other tweaks

--- scripts/install-demo.sh
+++ scripts/install-demo.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 composer config repositories.commerce_demo vcs https://github.com/mglaman/commerce_demo
 composer require drupal/commerce_demo *@dev
+echo $(pwd)
 ./bin/drush --root=$(pwd)/web en -y commerce_demo -y
 ./bin/drush --root=$(pwd)/web cr
 ./bin/drush --root=$(pwd)/web cc drush
